8491192004889406ee6adcd5c3b295191304639e
Yulia-Chamlai <yuliachamlai@gmail.com>
2022-12-19 10:30:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function navToggle() {
+  var navToggleNav = document.getElementById("nav");
+  navToggleNav.classList.toggle("_active");
+  var html = document.getElementById("html");
+  html.classList.toggle("_overflow");
+  var body = document.getElementById("body");
+  body.classList.toggle("_overflow");
+}
